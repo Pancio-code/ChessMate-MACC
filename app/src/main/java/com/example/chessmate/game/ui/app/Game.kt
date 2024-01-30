d083e85d46cd172a8dd32fc9f125bbbb9524ad52
Awenega <f.sudoso@gmail.com>
2024-01-30 18:22:09 +0100

    Added parser from text spoken to Position on the chessboard

--- app/src/main/java/com/example/chessmate/game/ui/app/Game.kt
+++ app/src/main/java/com/example/chessmate/game/ui/app/Game.kt
@@ -1,5 +1,6 @@
 package com.example.chessmate.game.ui.app
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -37,6 +38,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.chessmate.R
+import com.example.chessmate.game.model.board.Position
 import com.example.chessmate.game.model.data_chessmate.LocalActiveDatasetVisualisation
 import com.example.chessmate.game.model.game.controller.GameController
 import com.example.chessmate.game.model.game.preset.Preset
@@ -51,6 +53,7 @@ import com.example.chessmate.multiplayer.GameType
 import com.example.chessmate.multiplayer.OnlineUIClient
 import com.example.chessmate.multiplayer.OnlineViewModel
 import com.example.chessmate.sign_in.UserData
+import java.util.Locale
 
 @Composable
 fun Game(
@@ -134,7 +137,8 @@ fun Game(
                 onStepForward = { gameController.stepForward() },
                 onChessMateClicked = { showChessMateDialog.value = true },
                 onFlipBoard = { isFlipped = !isFlipped },
-                onGameClicked = { showGameDialog.value = true }
+                onGameClicked = { showGameDialog.value = true },
+                gameController = gameController
             )
 
             if (gamePlayState.value.gameState.gameMetaInfo.result != null && gamePlayState.value.gameState.gameMetaInfo.termination != null) {
@@ -231,6 +235,7 @@ private fun GameControls(
     onChessMateClicked: () -> Unit,
     onFlipBoard: () -> Unit,
     onGameClicked: () -> Unit,
+    gameController: GameController
 ) {
     var textSpoken by remember { mutableStateOf("") }
 
@@ -299,14 +304,81 @@ private fun GameControls(
     ){
         ButtonSpeechToText(setSpokenText = {textSpoken = it})
         if (textSpoken != "") {
+            Log.d("SpeechToText", textSpoken)
+            val position = parseSpeechToMove(textSpoken)
             Text(
                 modifier = Modifier.padding(top = 8.dp),
-                text = textSpoken,
+                text = position?.toString() ?: "Retry",
                 style = MaterialTheme.typography.bodySmall,
                 textAlign = TextAlign.Center,
                 color = MaterialTheme.colorScheme.secondary
             )
+            //if (position != null){
+                //gameController.onClick(position)
+            //}
         }
     }
 }
 
+private fun parseSpeechToMove(textSpoken: String): Position? {
+
+    val textSpokenParsed = parseString(textSpoken)
+    if (textSpokenParsed.isNullOrEmpty()){
+        return null
+    }
+
+    val position: Position? = try {
+        Position.valueOf(textSpokenParsed)
+    } catch (e: IllegalArgumentException) {
+        return null
+    }
+
+    return position
+}
+
+private fun parseString(textSpoken: String): String? {
+    val row: String
+    val column: String
+    if (textSpoken.length > 2 && textSpoken.contains(' ')){
+        if(textSpoken.split(" ")[0].length != 1) {
+            return null
+        } else {
+            row = textSpoken.split(" ")[0]
+            column = convertNumericWordsToDigits(textSpoken.split(" ")[1])
+        }
+    } else if (textSpoken.length > 2 && !textSpoken.contains(' ')){
+        row = textSpoken[0].toString()
+        column = convertNumericWordsToDigits(textSpoken.substring(1))
+    } else if (textSpoken.length == 2) {
+        row = textSpoken[0].toString()
+        column = textSpoken[1].toString()
+    } else {
+        return null
+    }
+    return "${row}${column}".lowercase()
+}
+private fun convertNumericWordsToDigits(textSpoken: String): String {
+
+    val wordToDigitMap = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "uno" to "1",
+        "due" to "2",
+        "tre" to "3",
+        "quattro" to "4",
+        "cinque" to "5",
+        "sei" to "6",
+        "sette" to "7",
+        "otto" to "8",
+    )
+
+    return textSpoken.split(" ").joinToString(" ") { word ->
+        wordToDigitMap[word.lowercase(Locale.getDefault())] ?: word
+    }
+}
